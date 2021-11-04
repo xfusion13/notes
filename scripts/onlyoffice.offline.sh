a4d8d35b03620d61caf154920e1fdf595c1540b0
xfusion13 <xfusion13@yandex.ru>
2021-11-04 21:03:50 +0300

    04.11.2021

---
+++ scripts/onlyoffice.offline.sh
@@ -0,0 +1,2 @@
+sudo dpkg -i ../../software/packages/onlyoffice-desktopeditors_amd64.deb
+sudo apt install -f
---
+++ scripts/vscode.offline.sh
@@ -0,0 +1,2 @@
+sudo dpkg -i ../../software/packages/code_1.61.2-1634656828_amd64.deb
+sudo apt install -f
